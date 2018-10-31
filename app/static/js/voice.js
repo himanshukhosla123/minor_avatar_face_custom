feb75dbfd20f6479a1211ce0bd7669ee47ab99d6
Himanshu khosla <himanshukhosla123@gmail.com>
2018-10-31 13:19:40 +0530

    dix

--- app/static/js/voice.js
+++ app/static/js/voice.js
@@ -2,7 +2,7 @@ var baseUrl = "https://myvoice.lyrebird.ai";
 var basrUrlAvatar="https://avatar.lyrebird.ai"
 var clientId = "1CJBAAIWXaFhfUxqB2lTQtga1sy";
 var clientSecret = "$2a$10$5yapuzRSX/GzEiVbQzlOneSWNc5IBlY7ivwVV8nohPdQhrZ2G0pUi";
-var encodedUrl="https%3A%2F%2Fhimanshukhosla123.github.io%2Fminor_avatar_face_custom%2Fapp%2Fstatic%2Findex.html";
+var encodedUrl="https%3A%2F%2Fhimanshukhosla123.github.io%2Fminor_avatar_face_custom%2Fapp%2Fstatic%2F";
 var token=localStorage.getItem("acs");
 var code="9818478695";
 
